e0f281da516213cf556b7ed1168a64ddabbe3972
Leweke Precious Chigozie <lewekeprecious8@gmail.com>
2026-07-25 12:28:04 +0100

    Create a PyQt5 application with a button and label

---
+++ 42.py
@@ -0,0 +1,36 @@
+
+#PyQt5 layouts
+#Introduction to push buttons in PyQt5
+
+
+import sys
+
+from PyQt5.QtCore import QRect
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(700, 300, 500, 500)
+        self.button = QPushButton("Click me!", self)
+        self.label = QLabel("Hello World!", self)
+        self.initUI()
+
+    def initUI(self):
+        self.button.setGeometry(150, 200, 200, 100)
+        self.button.setStyleSheet("font-size:30px; ")
+        self.button.clicked.connect(self.on_click)
+
+        self.label.setGeometry(150, 300, 200, 100)
+        self.label.setStyleSheet("font-size:30px; ")
+
+    def on_click(self):
+        self.label.setText("Goodbye!")
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
+```
